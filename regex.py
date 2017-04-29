6c7b150e00cb3968a73aa4bcd437fb22f000ebaf
funilrys <contact@funilrys.com>
2017-04-29 19:35:59 +0200

    Add licence + Review docstring

--- regex.py
+++ regex.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 
+#    python-regex - A simple implementation ot the python.re package
+#    Copyright (C) 2017  Nissar Chababy <contact at funilrys dot com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from re import compile, search
 
 
 class Regex(object):
-    """A simple implementation ot the python.re library"""
+    """A simple implementation ot the python.re package"""
 
     def __init__(self, data, regex, return_data=True, group=0):
         super(Regex, self).__init__()
